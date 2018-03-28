fc81b2fa96f9c7f96b348229c1b4108f5a49f775
Getty Ritter <gdritter@galois.com>
2018-03-28 15:43:31 -0700

    ...

--- setup_rust_env.sh
+++ setup_rust_env.sh
@@ -15,8 +15,8 @@ stdbuf -oL printf "Fetching sel4 sysroot..."
     rm -rf sysroot &&
         mkdir sysroot &&
         cd sysroot &&
-        curl -O https://infinitenegativeutility.com/i686-sel4-robigalia.tar.gz &&
-        tar -xf i686-sel4-robigalia.tar.gz
+        curl -O https://infinitenegativeutility.com/x86_64-sel4-robigalia.tar.gz &&
+        tar -xf x86_64-sel4-robigalia.tar.gz
 ) >/dev/null 2>&1
 echo " done."
 
@@ -24,7 +24,7 @@ stdbuf -oL printf "Setting up tree-local Cargo configuration..."
 mkdir -p .cargo
 cat >.cargo/config <<EOF
 [build]
-target = "i686-sel4-robigalia"
+target = "x86_64-sel4-robigalia"
 rustflags = ["--sysroot", "sysroot"]
 rustc = "rust/bin/rustc"
 EOF
